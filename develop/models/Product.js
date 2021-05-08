a2b83438a30ee07f74b0e84b387077b8ba6d5cd0
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-05-08 11:59:57 -0500

    Update Product.js
    worked on the Product.js file finished setting up the rules for the products

--- develop/models/Product.js
+++ develop/models/Product.js
@@ -4,13 +4,36 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGERS,
+      primaryKey: true,
+      allownull: false,
+      autoincrement: true
+    },
+
+    product_id: {
+      type: DataTypes.INTEGERS,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    
+    tag_id:{
+      tupe: DataTypes.INTEGERS,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
+
   {
     sequelize,
     timestamps: false,
