dc795d6ddd48b78614e3fbcf328f37def6062d9b
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-05-08 11:48:38 -0500

    Update index.js
    worked on the index.js

--- develop/models/index.js
+++ develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey:'category_id',
+});
 
 // Categories have many Products
+Category.hasmany(Product, {
+  foreignKey: 'catergory_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongstomany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongstomany(Product, {
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
