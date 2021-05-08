680c95d3fe064049c06a40491ed8bd8c590f320c
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-05-08 13:44:36 -0500

    added a comment on index.js
    comment is located in the routes/api folder

--- develop/routes/api/index.js
+++ develop/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
-
+// could i use router.use('/api,apiroutes)?
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
