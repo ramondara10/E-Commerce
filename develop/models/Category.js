6ee37a4ae9716fb758d1f57d7a35c3df29264782
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-05-08 20:07:15 -0500

    cleaning code

--- develop/models/Category.js
+++ develop/models/Category.js
@@ -8,7 +8,7 @@ Category.init(
   {
     // define columns
     id:{
-        type: datatypes.INTEGERS,
+        type: dataTypes.INTEGERS,
         allownull: false,
         primarykey: true,
         autoincrement: true
